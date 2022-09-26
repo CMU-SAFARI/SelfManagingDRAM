8d45816a6dafdb47ba35db53c4f42c62da97b0d3
Ataberk <olgunataberk@gmail.com>
2022-09-26 11:27:34 +0200

    add descriptions to run.sh config parameters

--- run.sh
+++ run.sh
@@ -55,29 +55,28 @@ configs["timeout_row_policy_threshold"]=360
 # Configuration parameter updates applied after the warmup
 declare -A sim_configs
 
-sim_configs["disable_refresh"]="false" # disabling conventional refresh
-sim_configs["per_bank_refresh"]="false"
-ref_mult="0.5f"
+sim_configs["disable_refresh"]="false" # disable/enable conventional refresh
+sim_configs["per_bank_refresh"]="false" # disable/enable per bank refresh
+ref_mult="0.5f" # multiply the default refresh interval (64 ms) by this number
 sim_configs["refresh_mult"]=${ref_mult}
 
 # Enable SMD-Combined: SMD-VR, SMD-DRP, and SMD-MS
-sim_configs["smd"]=on
-configs["smd_refresh_period"]=51200000 # // 51,200,000 cycles = 32ms at 1600Mhz (i.e., 3200 data rate)
-configs["smd_ref_policy"]=VariableRefresh
-configs["smd_variable_refresh_weak_row_percentage"]="0.1"
+sim_configs["smd"]=on # enable/disable SMD
+configs["smd_refresh_period"]=51200000 # 51,200,000 cycles = 32ms at 1600Mhz (i.e., 3200 data rate)
+configs["smd_ref_policy"]=VariableRefresh # variable rate refresh ("VariableRefresh") or fixed rate refresh ("FixedRefresh")
+configs["smd_variable_refresh_weak_row_percentage"]="0.1" # the proportion of retention-weak rows among all rows simulated
 
-configs["smd_rh_protection_enabled"]=true
-configs["smd_rh_protection_mac"]=1024
-configs["smd_rh_neighbor_refresh_pct"]=1
-configs["smd_rh_protection_mode"]="Graphene"
+configs["smd_rh_protection_enabled"]=true # disable/enable SMD deterministic/probabilistic RowHammer protection
+configs["smd_rh_protection_mac"]=1024 # ACTmax threshold for SMD RowHammer protection
+configs["smd_rh_neighbor_refresh_pct"]=1 # (0-100) the probability of neighbor row refresh to occur when a row is activated (relevant for probabilistic protection)
+configs["smd_rh_protection_mode"]="Graphene" # deterministic ("Graphene") or probabilistic ("PARA") RowHammer protection
 
-configs["smd_ecc_scrubbing_enabled"]=true
-configs["smd_num_scrubbing_machines"]=1
-configs["smd_timeout_to_ecc_scrubbing_interval_ratio"]=1
-configs["smd_scrub_granularity"]=1
-configs["smd_single_scrub_latency"]=552
-configs["smd_scrubbing_lock_region"]=bank
-configs["smd_ecc_scrubbing_period"]=5760000000000 # 5 minute scrubbing period
+configs["smd_ecc_scrubbing_enabled"]=true # enable/disable ECC scrubbing (SMD-MS)
+configs["smd_num_scrubbing_machines"]=1 # the number of banks that are scrubbed concurrently
+configs["smd_scrubbing_granularity"]=1 # number of rows scrubbed within a single smd region lock
+configs["smd_single_scrubbing_latency"]=552 # simulated latency (in number of memory controller cycles) for a scrub operation
+configs["smd_scrubbing_lock_region"]=bank # valid entries: 'bank' and 'region'. When bank, scrubbing puts an entire bank under maintenance whereas region operates as in smd refresh
+configs["smd_ecc_scrubbing_period"]=5760000000000 # 5 minute scrubbing period, the time it takes for the scrubber to process all DRAM rows
 
 # start the simulation
 cmd="./ramulator ./configs/SMD_configs/DDR4.cfg --mode=cpu -t ${TRACE_DIR}/${workload_name} $(prep_config_params)"
